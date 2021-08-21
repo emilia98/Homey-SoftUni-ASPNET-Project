4c6d2f281dd108575fc174788acd357beac7a399
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2021-08-21 23:02:16 +0300

    refactor authentication

--- Homey/Web/Homey.API/Controllers/AuthController.cs
+++ Homey/Web/Homey.API/Controllers/AuthController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Homey.Data.Models;
 using Homey.InputModels.Auth;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
--- Homey/Web/Homey.API/Startup.cs
+++ Homey/Web/Homey.API/Startup.cs
@@ -63,6 +63,7 @@ namespace Homey.API
 
             app.UseRouting();
 
+            // app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
