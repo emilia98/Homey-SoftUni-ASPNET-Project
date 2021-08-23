ab4cef4198004ddd2a1fece1db82e835c24b57f5
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2021-08-23 21:27:11 +0300

    add property type controller

---
+++ Homey/Web/Homey.API/Controllers/PropertyTypeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Homey.OutputModels;
+using Homey.Services.Data.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homey.API.Controllers
+{
+    [Route("propertyType")]
+    [AllowAnonymous]
+    public class PropertyTypeController : Controller
+    {
+        private readonly IPropertyTypeService propertyTypeService;
+
+        public PropertyTypeController(IPropertyTypeService propertyTypeService)
+        {
+            this.propertyTypeService = propertyTypeService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var propertyType = propertyTypeService.GetById<PropertyTypeOutputModel>(id);
+
+            if (propertyType == null)
+            {
+                return NotFound(new
+                {
+                    ErrorMsg = "Property type not found!"
+                });
+            }
+
+            return Ok(new
+            {
+                PropertyType = propertyType
+            });
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            var propertyTypes = propertyTypeService.GetAll<List<PropertyTypeOutputModel>>();
+            return Ok(new
+            {
+                PropertyTypes = propertyTypes
+            });
+        }
+    }
+}
---
+++ Homey/Web/Homey.OutputModels/PropertyTypeOutputModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Homey.OutputModels
+{
+    public class PropertyTypeOutputModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
